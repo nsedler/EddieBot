6180b11b22b30d88f73ec2b952508dbe1e5f3305
Nate Sedler <nsedler1@gmail.com>
2018-10-18 09:03:33 -0400

    Update Constants.java

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -2,6 +2,6 @@ package main.java;
 
 public class Constants {
 
-    public static final String discordToken = "NDk4NjA5NTEwNzUxOTI4MzMw.Dp7yzQ.0RLc-uTa896gF3zldVoECSGJ5WU";
+    public static final String discordToken = ";
 }
 
