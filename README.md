81258225f43a97d25d2d1aebeb413afff83ab248
nsedler <nsedler1@gmail.com>
2018-10-18 02:53:19 -0400

    i want to die

--- README.md
+++
@@ -1,2 +0,0 @@
-# EddieBot
-Discord bot 
---
+++ src/main/java/Constants.java
@@ -0,0 +1,7 @@
+package main.java;
+
+public class Constants {
+
+    public static final String discordToken = "NDk4NjA5NTEwNzUxOTI4MzMw.Dp7yzQ.0RLc-uTa896gF3zldVoECSGJ5WU";
+}
+
