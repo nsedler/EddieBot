d619174c6b1c33e4370a8e8a15b99743be481b1a
Nate Sedler <nsedler1@gmail.com>
2018-10-21 18:42:42 -0400

    hosting main bot

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -24,7 +24,7 @@ public class main extends ListenerAdapter {
 
 
         try {
-            discord = new JDABuilder(AccountType.BOT).setToken(System.getenv("test_token")).buildBlocking();
+            discord = new JDABuilder(AccountType.BOT).setToken(System.getenv("token")).buildBlocking();
         } catch (LoginException | IllegalArgumentException | InterruptedException e) {
             e.printStackTrace();
         }
