9be6cb47e5d472445b0b8328a29019623656098f
Nate Sedler <nsedler1@gmail.com>
2018-10-22 12:55:15 -0400

    added jail command

--- src/main/java/Administrator/Jail.java
+++ src/main/java/Administrator/Jail.java
@@ -5,10 +5,6 @@ import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
-import net.dv8tion.jda.core.managers.GuildController;
-import org.apache.maven.shared.utils.StringUtils;
-
-import java.util.*;
 
 public class Jail extends ListenerAdapter {
 
@@ -18,12 +14,11 @@ public class Jail extends ListenerAdapter {
         MessageChannel channel = event.getChannel();
         Member toMute = event.getMessage().getMentionedMembers().get(0);
         Role role = event.getGuild().getRoleById("499676837316919316");
-        List<Role> roles = event.getMember().getRoles();
         String[] command = event.getMessage().getContentDisplay().split(" ", 2);
 
 
         if(".jail".equalsIgnoreCase(command[0])){
-            event.getGuild().getController().removeRolesFromMember(toMute, roles).complete();
+            event.getGuild().getController().removeRolesFromMember(toMute, toMute.getRoles()).complete();
             event.getGuild().getController().addSingleRoleToMember(toMute, role).complete();
 
             channel.sendMessage(command[1] + " was muted by " + event.getAuthor().getName()).queue();
