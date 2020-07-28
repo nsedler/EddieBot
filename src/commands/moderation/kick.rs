9f2b4896cb4fcaa77e630cfb1f28c2d50859c704
Nate Sedler <nsedler1@gmail.com>
2020-07-28 01:36:06 -0400

    Added err handing for kick cmd

--- src/commands/moderation/kick.rs
+++ src/commands/moderation/kick.rs
@@ -1,5 +1,4 @@
 use crate::commands::command::Command;
-use serenity::model::id::UserId;
 use serenity::{model::channel::Message, prelude::*};
 use std::io;
 
@@ -19,26 +18,44 @@ impl Command for Kick {
         false
     }
     fn execute(&self, ctx: &Context, msg: &Message) -> io::Result<()> {
-
         let kicked = &msg.mentions;
-        let guild = msg.guild_id.unwrap().to_partial_guild(&ctx.http).unwrap();
+        // let guild = msg.guild_id.unwrap().to_partial_guild(&ctx.http).unwrap();
+        let guild = msg.guild(&ctx).unwrap().read().clone();
         let mut kicked_users = String::new();
 
-        for user in kicked {
-            kicked_users.push_str(
-                format!("{}#{} Id: {}\n", user.name, user.discriminator, user.id).as_str(),
-            );
-            guild
-                .kick(&ctx.http, user)
-                .expect(format!("Err at kicking player{}", user.name).as_str());
+        if !kicked.is_empty() {
+            for user in kicked {
+                let kick_result = guild.kick(&ctx, user);
+                match kick_result {
+                    Err(err) => {
+                        msg.channel_id
+                            .say(
+                                &ctx.http,
+                                format!("An unknown error has caused this command to fail: {}", err),
+                            )
+                            .expect("Failed at unknown error");
+                            break;
+                    }
+                    Ok(_) => {
+                        kicked_users.push_str(
+                            format!("{}#{} Id: {}\n", user.name, user.discriminator, user.id).as_str(),
+                        );
+                        msg.channel_id
+                        .say(
+                            &ctx.http,
+                            format!("{} has been kicked from the guild.", kicked_users),
+                        )
+                        .expect("Ban command failed");
+                    }
+                }
+            }
+            
+        } else {
+            msg.channel_id
+                .say(&ctx.http, "You must mention the user you wish to ban.")
+                .expect("No user to kick mentioned");
         }
-        
-        msg.channel_id
-            .say(
-                &ctx.http,
-                format!("{} has been kicked from the guild.", kicked_users),
-            )
-            .expect("Ban command failed");
+
         Ok(())
     }
 }
