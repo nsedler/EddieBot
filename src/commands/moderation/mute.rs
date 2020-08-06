46868e07c0a359f8be514cbcc70f300fffe7b1d1
Nate Sedler <nsedler1@gmail.com>
2020-08-05 20:49:57 -0400

    Started mute command, got bored, moving onto something else

--- src/commands/moderation/mute.rs
+++ src/commands/moderation/mute.rs
@@ -1,3 +1,6 @@
+extern crate regex;
+
+use regex::Regex;
 use crate::commands::command::Command;
 use serenity::model::guild::Guild;
 use serenity::{model::channel::Message, prelude::*};
@@ -19,11 +22,14 @@ impl Command for Mute {
         false
     }
     fn execute(&self, ctx: &Context, msg: &Message) -> io::Result<()> {
+        let time_pattern = Regex::new(r"\s+[0-9]+(m|h|d)").unwrap();
         let guild: &Guild = &msg.guild(&ctx).unwrap().read().clone();
         let guild_members = &guild.members;
         let guild_roles = &guild.roles;
         let member = guild_members.get(&msg.mentions[0].id).unwrap();
         let guild_has_mute_role = guild_roles.values().any(|val| val.name == "mute");
+        let mute_length = time_pattern.captures(msg.content.as_str()).unwrap()[0].to_string();
+        println!("{:?}", mute_length);
         if guild_has_mute_role {
             let role = guild_roles
                 .iter()
@@ -50,7 +56,16 @@ impl Command for Mute {
                 }
             }
         } else {
-            todo!();
+            let create_mute_role = guild.create_role(&ctx.http, |r| r.hoist(true).name("mute").colour(999999));
+
+            match create_mute_role {
+                Err(err) => {
+                    msg.channel_id.say(&ctx.http, format!("An error has occured: `{}`", err)).expect("Error creating new role");
+                }
+                Ok(role) => {
+                    member.clone().add_role(&ctx.http, role.id).expect("Created and assigned mute role");
+                }
+            }
         }
         Ok(())
     }
